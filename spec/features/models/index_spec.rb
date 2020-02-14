e9d3ad31fc585b75a9175146f70c000cedbd9002
Pierce Alworth <pbalworth@gmail.com>
2020-02-14 10:13:46 -0700

    Minor styling for US 1

--- spec/features/models/index_spec.rb
+++ spec/features/models/index_spec.rb
@@ -6,22 +6,15 @@ RSpec.describe " When I visit studio index" do
     studio_2 = Studio.create!(name: "Fun Videos")
     movie_1 = studio_1.movies.create!(name: "BIG", year:1996, genre: "Horror")
     movie_2 = studio_2.movies.create!(name: "Crash", year:2004, genre: "Drama")
-    # studio_1.movies << movie_1
-    # studio_2.movies << movie_2
 
       visit '/studios'
       expect(page).to have_content(studio_1.name)
       expect(page).to have_content(movie_1.name)
       expect(page).to have_content(studio_2.name)
       expect(page).to have_content(movie_2.name)
+      save_and_open_page
+      
 
   end
 
 end
-
-
-
-
-# When I visit the studio index page
-# I see a list of all of the movie studios
-# And underneath each studio, I see the names of all of its movies.
